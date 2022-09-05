448dd72af201c1ef7fc424250fcfc1bf00cb40a6
gregorio4g <gregorio4g@gmail.com>
2022-09-05 13:07:17 +0200

    zadania 2

--- src/com/gg/Main.java
+++ src/com/gg/Main.java
@@ -1,7 +1,38 @@
 package com.gg;
 
 public class Main {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("2. Napisz taki sam program, tylko spraw, żeby na ekranie została wydrukowana treść tego zadania.");
+        String abc$12 = "a";
+        String Private = "b";
+        String _17 = "c";
+        System.out.println(abc$12 + Private + _17);
+        double points = 14.4;
+        String nextPoints = "16.2";
+        System.out.print(points + 1);
+        System.out.print(" ");
+        System.out.print(nextPoints + 1);
+        String sentence = "JavaCoding!";
+        System.out.print(sentence.length());
+        System.out.print(sentence.charAt(2));
+        System.out.print(sentence.charAt(7));
+        System.out.println();
+        String a = "JavaCoding! ";
+        a = a + 2;
+        a = a + "world";
+        System.out.println(a.length() + a);
+        System.out.println();
+
+
+
+
+    }
+
+    public void makeNoise() {
+
+        {
+            String result = "result";System.out.println(result);
+        }
+
     }
 }
