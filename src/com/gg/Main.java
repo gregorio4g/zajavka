49d88fe1455a9d77b87cb8ecfda2881fa3452ed8
gregorio4g <gregorio4g@gmail.com>
2022-09-12 01:12:23 +0200

    zadania 5

--- src/com/gg/Main.java
+++ src/com/gg/Main.java
@@ -51,11 +51,98 @@ public class Main {
         int num4_3 = 66;
         System.out.println("Liczba " + num4_3 + (czyParzysta(num4_3)?"":" nie") + " jest parzysta.");
         // exc 4 - zad 4
-        System.out.printf("Marzec ma %d dni.\n", howManyDaysInMonth(13));
+        System.out.printf("Marzec ma %d dni.\n", howManyDaysInMonth(3));
         // exc 4 - zad 5
-        System.out.printf("Od początku roku do końca marca jest %d dni.\n", howManyDaysInYear(13));
+        System.out.printf("Od początku roku do końca marca jest %d dni.\n", howManyDaysInYear(3));
+        // exc 5 - zad 1
+        int num5_1 = 5;
+        System.out.printf("%d! = %d%n", num5_1, silnia(num5_1));
+        // exc 5 - zad 2
+        String s5_2 = "łaPAł";
+        System.out.printf("Słowo: %s " + (palindrom(s5_2) ? "" : "nie ") + "jest palindromem.%n", s5_2);
+        // exc 5 - zad 3
+        christmasTree(7);
+        // exc 5 - zad 4
+        String[] colours = {"red", "grn", "blu", "blk", "yel", "brn"};
+        for (int i = 0; i < 100; i++) {
+            System.out.print(colours[i%6]+" ");
+            if ((i+1)%25 == 0) System.out.println();
+        }
+        // exc 5 - zad 5
+        // java-Deitel: c625
+        // exc 5 - zad 6
+        System.out.println(printNumbers(19));
+        // exc 5 - zad 7
+        System.out.println(fibonacci(20));
+        // exc 5 - zad 8
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i * 100 + i * 10 + i);
+            for (int j = 0; j < 4; j++) {
+                System.out.println("####");
+            }
+        }
 
     }
+
+    static String fibonacci(int len) {
+        StringBuilder output = new StringBuilder();
+        int num1 = 1;
+        int num2 = 1;
+        int num3;
+        output.append("1,1,");
+        for (int i = 0; i < len - 2; i++) {
+            num3 = num1 + num2;
+            output.append(num3).append(i == len - 3 ? "" : " ");
+            num1 = num2;
+            num2 = num3;
+        }
+        return output.toString();
+    }
+
+    static String printNumbers(int length) {
+        StringBuilder output = new StringBuilder();
+        int num1 = 39;
+        int num2 = 11;
+        for (int i = 0; i < length/2; i++) {
+            output.append(num1).append(",").append(num2);
+            output.append((i == length/2 - 1 ? "" : ","));
+            num1 += 2;
+            num2 -= 3;
+        }
+        if (length % 2 == 1) {
+            output.append(",").append(num1);
+        }
+        return output.toString();
+    }
+
+    static void christmasTree(int num) {
+        int start = (num % 2 == 0 ? 2 : 1);
+        for (int i = start; i <= num; i += 2) {
+            for (int j = 0; j < (num-i)/2; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+
+    static boolean palindrom(String input) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = input.length()-1; i >= 0 ; i--) {
+            sb.append(input.charAt(i));
+        }
+        return (input.equalsIgnoreCase(sb.toString()));
+    }
+    static long silnia(int i) {
+        long result = 1;
+        for (int j = 2; j <= i; j++) {
+            result *= j;
+        }
+        return result;
+    }
+
     static void zad3_1(double x, double y) {
         System.out.printf("%.2f + %.2f = %.2f\n",x, y, (x + y));
         System.out.printf("%.2f - %.2f = %.2f\n",x, y, (x - y));
