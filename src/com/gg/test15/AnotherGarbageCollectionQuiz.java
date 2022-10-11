dc5260221b35baf391da4f06e85b4238b78fa71d
gregorio4g <gregorio4g@gmail.com>
2022-10-12 00:24:23 +0200

    test 15

---
+++ src/com/gg/test15/AnotherGarbageCollectionQuiz.java
@@ -0,0 +1,16 @@
+package com.gg.test15;
+
+public class AnotherGarbageCollectionQuiz {
+	public static void main(String[] args) {
+		AnotherGarbageCollectionQuiz one = new AnotherGarbageCollectionQuiz();
+		AnotherGarbageCollectionQuiz two = new AnotherGarbageCollectionQuiz();
+		AnotherGarbageCollectionQuiz three = two;
+		two = null;
+		AnotherGarbageCollectionQuiz four = one;
+		three = null;
+		one = null;
+		four = null;
+		one = new AnotherGarbageCollectionQuiz();
+		System.gc();
+	}
+}
