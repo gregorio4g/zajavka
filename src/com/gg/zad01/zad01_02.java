4b305ed050bebb39a6ad328636c8bb6d03e01f50
gregorio4g <gregorio4g@gmail.com>
2024-05-20 00:14:34 +0200

    zad 01

---
+++ src/com/gg/zad01/zad01_02.java
@@ -0,0 +1,7 @@
+package com.gg.zad01;
+
+public class zad01_02 {
+    public static void main(String[] args) {
+        System.out.println("Napisz taki sam program, tylko spraw, żeby na ekranie została wydrukowana treść tego zadania.");
+    }
+}
