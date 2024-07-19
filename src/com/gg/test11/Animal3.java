6182e2e7e2be9a02146548c9f702be5deab885dd
gregorio4g <gregorio4g@gmail.com>
2024-07-19 23:17:02 +0200

    day 27

--- src/com/gg/test11/Animal3.java
+++ src/com/gg/test11/Animal3.java
@@ -1,17 +1,21 @@
 package com.gg.test11;
 
-public enum Animal implements CanSwim {
+public enum Animal3 {
     DOG(true),
     CAT(false),
     BIRD(false),
     FISH(true);
+
     private boolean canSwim;
-    Animal(boolean canSwim) {
+
+    Animal3(boolean canSwim) {
         this.canSwim = canSwim;
     }
+
     public boolean canSwim(){
         return canSwim;
     }
+
     public void learnToSwim(){
         this.canSwim = true;
     }
