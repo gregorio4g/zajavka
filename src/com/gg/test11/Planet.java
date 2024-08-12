3b478e55c5f8c903fdfb0593df57469300207042
105053053 <Grzegorz.Gralczyk1@ge.com>
2024-08-12 07:45:26 +0200

    test 11 Planet

---
+++ src/com/gg/test11/Planet.java
@@ -0,0 +1,18 @@
+package com.gg.test11;
+
+public enum Planet {
+//    MECURY,
+//    VENUS,
+    EARTH {
+        public boolean isInhabited() {
+            return true;
+        }
+    };
+//    MARS,
+//    JUPITER,
+//    SATURN,
+//    URANUS,
+//    NEPTUNE;
+
+    public abstract boolean isInhabited();
+}
